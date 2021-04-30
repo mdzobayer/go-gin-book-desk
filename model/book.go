b33b131d55e80666c09d4e9f0d1346c8445b5537
mdzobayer <mdzobayer@rocketmail.com>
2021-04-30 19:38:05 +0600

    Id property added on book model

--- model/book.go
+++ model/book.go
@@ -1,7 +1,10 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Book struct {
-	Title       string `json:"Title"`
-	Description string `json:"Description"`
-	ISBN        string `json:"ISBN"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title       string             `json:"Title"`
+	Description string             `json:"Description"`
+	ISBN        string             `json:"ISBN"`
 }
